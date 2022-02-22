45ec0e209e5c0ac6cd59445c707ec09c889aa32f
caternuson <caternuson@gmail.com>
2022-02-21 18:48:44 -0800

    basic samd10 blink

--- boards/heehaw10/configboard.mk
+++ boards/heehaw10/configboard.mk
@@ -1,9 +1,5 @@
 BOARD_NAME = heehaw10
+CHIP_VARIANT = ATSAMD10D14AM
 CHIP_FAMILY = samd10
-CHIP = __ATSAMD10D14AM__
 LINKER_SCRIPT = ldscripts/samd10d14am_flash.ld
 
-ARM_CPU   := -mcpu=cortex-m0plus
-ARM_ARCH  := 6   # NOTE: must match the ARM_CPU!
-ARM_FPU   :=
-FLOAT_ABI :=
